9fda574152a34cb1d42c266acfd18cef74bc200c
Paul Smith <paulcsmith0218@gmail.com>
2017-06-09 17:10:41 -0400

    Less webpack output in development

--- src/generators/templates/webpack_config/webpack/development.js
+++ src/generators/templates/webpack_config/webpack/development.js
@@ -6,6 +6,17 @@ const sharedConfig = require('./shared.js')
 module.exports = merge(sharedConfig, {
   devtool: 'cheap-eval-source-map',
 
+  stats: {
+    assets: false,
+    chunkModules: false,
+    chunks: false,
+    colors: true,
+    hash: false,
+    maxModules: 0,
+    timings: false,
+    version: false
+  },
+
   output: {
     pathinfo: true
   }
