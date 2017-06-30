ba0b308af1f3ebccec715e9073b2e1aed636a7ca
Paul Smith <paulcsmith0218@gmail.com>
2017-06-30 09:36:58 -0400

    Add StaticFileHandler

--- src/generators/templates/project_src/src/server.cr
+++ src/generators/templates/project_src/src/server.cr
@@ -5,6 +5,7 @@ server = HTTP::Server.new("127.0.0.1", 8080, [
   HTTP::ErrorHandler.new,
   HTTP::LogHandler.new,
   LuckyWeb::RouteHandler.new,
+  HTTP::StaticFileHandler.new("./public", false),
 ])
 
 puts "Listening on http://127.0.0.1:8080...".colorize(:green)
