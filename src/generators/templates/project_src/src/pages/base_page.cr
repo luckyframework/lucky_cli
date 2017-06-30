3c2b52e10488e951ce20c3cd6690e91ce24b3e66
Paul Smith <paulcsmith0218@gmail.com>
2017-06-30 09:36:32 -0400

    Add page title, css_link and js_link to layout

--- src/generators/templates/project_src/src/pages/base_page.cr
+++ src/generators/templates/project_src/src/pages/base_page.cr
@@ -9,4 +9,8 @@ abstract class BasePage
 
     layout MainLayout
   end
+
+  def page_title
+    "Lucky - change at src/pages/base_page.cr"
+  end
 end
--- src/generators/templates/project_src/src/pages/main_layout.cr
+++ src/generators/templates/project_src/src/pages/main_layout.cr
@@ -1,10 +1,22 @@
-class MainLayout
+class MainLayout < BasePage
   include LuckyWeb::Layout
 
   @page : BasePage
 
   render do
-    h1 "What a wonderful day"
-    @page.render_inner
+    html_doctype
+
+    html do
+      head do
+        title @page.page_title
+        css_link asset("app.css")
+        js_link asset("app.js")
+      end
+
+      body do
+        h1 "What a wonderful day"
+        @page.render_inner
+      end
+    end
   end
 end
