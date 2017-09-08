8df428f94eecc0d605289c22f33a4a6c920f6625
Paul Smith <paulcsmith0218@gmail.com>
2017-09-08 14:52:49 -0400

    Add errors_for to BasePage
    Closes #30

--- src/generators/templates/project_src/src/pages/base_page.cr
+++ src/generators/templates/project_src/src/pages/base_page.cr
@@ -13,4 +13,13 @@ abstract class BasePage
   def page_title
     "Lucky - change at src/pages/base_page.cr"
   end
+
+  def errors_for(field : LuckyRecord::AllowedField)
+    # Customize the markup and styles to match your application
+    unless field.valid?
+      div class: "error" do
+        text "#{field.name.to_s.capitalize} #{field.errors.join(", ")}"
+      end
+    end
+  end
 end
