7509c074439f6978eb1f1a4e0b73f45153eb23bb
Paul Smith <paulcsmith0218@gmail.com>
2017-10-20 23:15:30 -0400

    Update BasePage documentation
    Closes #32

--- src/web_app_skeleton/src/pages/base_page.cr
+++ src/web_app_skeleton/src/pages/base_page.cr
@@ -1,12 +1,12 @@
 abstract class BasePage
   include LuckyWeb::Page
 
-  macro inherited
-    # You can put assigns here that all pages need
-    #
-    # Example:
-    #   assign current_user : User
+  # You can put assigns here that all pages need
+  #
+  # Example:
+  #   assign current_user : User
 
+  macro inherited
     layout MainLayout
   end
 
