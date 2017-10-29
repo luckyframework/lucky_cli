5e9b2642e44adea75f66b76161b91960b426e970
Bob Zoller <bob@zoller.us>
2017-10-29 13:00:14 -1000

    compress static files

--- src/web_app_skeleton/src/handlers/static_file_handler.cr
+++ src/web_app_skeleton/src/handlers/static_file_handler.cr
@@ -1,4 +1,17 @@
-class HTTP::StaticFileHandler
+require "http"
+
+class StaticFileHandler < HTTP::StaticFileHandler
+  @compress_handler : HTTP::Handler = HTTP::CompressHandler.new
+
+  private def should_compress?(context)
+    (context.response.headers["Content-Length"]? || 0).to_i >= 1400 # assuming an MTU of 1500 bytes, compress responses that span multiple packets
+  end
+
+  def call(context)
+    super(context)
+    @compress_handler.call(context) if should_compress? context
+  end
+
   private def mime_type(path)
     case File.extname(path)
     when ".txt"          then "text/plain"
