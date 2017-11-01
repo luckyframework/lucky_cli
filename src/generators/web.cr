c6f59d16b796b8b7a68330da07a8fedb4717108c
Paul Smith <paulcsmith0218@gmail.com>
2017-10-31 21:39:28 -0400

    Use Lucky master until new version is released

--- src/generators/web.cr
+++ src/generators/web.cr
@@ -80,7 +80,7 @@ class LuckyCli::Generators::Web
     dependencies:
       lucky_web:
         github: luckyframework/web
-        version: ~> 0.4.2
+        branch: master
       lucky_migrator:
         github: luckyframework/migrator
         version: ~> 0.2.3
