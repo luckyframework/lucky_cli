cd7fc94aaccd1f52f52ae58725d7a99eae6242b6
Paul Smith <paulcsmith0218@gmail.com>
2017-10-31 23:50:18 -0400

    Handle JSON::ParseException error
    Closes #59

--- src/web_app_skeleton/src/actions/errors/show.cr
+++ src/web_app_skeleton/src/actions/errors/show.cr
@@ -1,4 +1,12 @@
 class Errors::Show < LuckyWeb::ErrorAction
+  def handle_error(error : JSON::ParseException)
+    if json?
+      json({error: "There was a problem parsing the JSON. Please check that it is formed correctly"}, status: 400)
+    else
+      head status: 400
+    end
+  end
+
   def handle_error(error : Exception)
     head status: 500
   end
