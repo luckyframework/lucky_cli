67c2b4305f0ff84a513a73a8b5eae72cdab7d4be
Paul Smith <paulcsmith0218@gmail.com>
2017-11-11 16:02:21 -0500

    Use latest LuckyWeb

--- src/generators/web.cr
+++ src/generators/web.cr
@@ -98,7 +98,7 @@ class LuckyCli::Generators::Web
     dependencies:
       lucky_web:
         github: luckyframework/web
-        version: ~> 0.5.0
+        branch: master
       lucky_migrator:
         github: luckyframework/migrator
         version: ~> 0.2.3
