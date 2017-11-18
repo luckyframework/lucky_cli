0098ee7353e0d0afe6aeb64e6f3bd0c5f4880732
Paul Smith <paulcsmith0218@gmail.com>
2017-11-18 13:24:09 -0500

    Switch to LuckyWeb::StaticFileHandler

--- src/web_app_skeleton/src/server.cr
+++ src/web_app_skeleton/src/server.cr
@@ -7,7 +7,7 @@ server = HTTP::Server.new("127.0.0.1", 8080, [
   LuckyWeb::Flash::Handler.new,
   LuckyWeb::ErrorHandler.new(action: Errors::Show),
   LuckyWeb::RouteHandler.new,
-  HTTP::StaticFileHandler.new("./public", false),
+  LuckyWeb::StaticFileHandler.new("./public", false),
 ])
 
 puts "Listening on http://127.0.0.1:8080..."
