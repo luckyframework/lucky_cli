3c05fb5127f7b054d4bdae9199e53961d6cb7f2d
Paul Smith <paulcsmith0218@gmail.com>
2017-11-28 21:35:45 -0500

    Format bs-config.json with Prettier

--- src/web_app_skeleton/bs-config.js
+++ src/web_app_skeleton/bs-config.js
@@ -1,4 +1,3 @@
-
 /*
  |--------------------------------------------------------------------------
  | Browser-sync config file
@@ -13,103 +12,101 @@
  |
  */
 module.exports = {
-    snippetOptions: {
-      rule: {
-        match: /<\/head>/i,
-        fn: function (snippet, match) {
-          return snippet + match;
-        }
+  snippetOptions: {
+    rule: {
+      match: /<\/head>/i,
+      fn: function(snippet, match) {
+        return snippet + match;
       }
-    },
-    "ui": {
-        "port": 3001,
-        "weinre": {
-            "port": 8080
-        }
-    },
-    "files": ["public/css/**/*.css", "public/js/**/*.js"],
-    "watchEvents": [
-        "change"
-    ],
-    "watchOptions": {
-        "ignoreInitial": true
-    },
-    "server": false,
-    "proxy": "localhost:8080",
-    "port": 3000,
-    "middleware": false,
-    "serveStatic": [],
-    "ghostMode": {
-        "clicks": true,
-        "scroll": true,
-        "location": true,
-        "forms": {
-            "submit": true,
-            "inputs": true,
-            "toggles": true
-        }
-    },
-    "logLevel": "info",
-    "logPrefix": "Browsersync",
-    "logConnections": false,
-    "logFileChanges": true,
-    "logSnippet": true,
-    "rewriteRules": [],
-    "open": "local",
-    "browser": "default",
-    "cors": false,
-    "xip": false,
-    "hostnameSuffix": false,
-    "reloadOnRestart": false,
-    "notify": true,
-    "scrollProportionally": true,
-    "scrollThrottle": 0,
-    "scrollRestoreTechnique": "window.name",
-    "scrollElements": [],
-    "scrollElementMapping": [],
-    "reloadDelay": 0,
-    "reloadDebounce": 0,
-    "reloadThrottle": 0,
-    "plugins": [],
-    "injectChanges": true,
-    "startPath": null,
-    "minify": true,
-    "host": null,
-    "localOnly": false,
-    "codeSync": true,
-    "timestamps": true,
-    "clientEvents": [
-        "scroll",
-        "scroll:element",
-        "input:text",
-        "input:toggles",
-        "form:submit",
-        "form:reset",
-        "click"
-    ],
-    "socket": {
-        "socketIoOptions": {
-            "log": false
-        },
-        "socketIoClientConfig": {
-            "reconnectionAttempts": 50
-        },
-        "path": "/browser-sync/socket.io",
-        "clientPath": "/browser-sync",
-        "namespace": "/browser-sync",
-        "clients": {
-            "heartbeatTimeout": 5000
-        }
-    },
-    "tagNames": {
-        "less": "link",
-        "scss": "link",
-        "css": "link",
-        "jpg": "img",
-        "jpeg": "img",
-        "png": "img",
-        "svg": "img",
-        "gif": "img",
-        "js": "script"
     }
+  },
+  ui: {
+    port: 3001,
+    weinre: {
+      port: 8080
+    }
+  },
+  files: ["public/css/**/*.css", "public/js/**/*.js"],
+  watchEvents: ["change"],
+  watchOptions: {
+    ignoreInitial: true
+  },
+  server: false,
+  proxy: "localhost:8080",
+  port: 3000,
+  middleware: false,
+  serveStatic: [],
+  ghostMode: {
+    clicks: true,
+    scroll: true,
+    location: true,
+    forms: {
+      submit: true,
+      inputs: true,
+      toggles: true
+    }
+  },
+  logLevel: "info",
+  logPrefix: "Browsersync",
+  logConnections: false,
+  logFileChanges: true,
+  logSnippet: true,
+  rewriteRules: [],
+  open: "local",
+  browser: "default",
+  cors: false,
+  xip: false,
+  hostnameSuffix: false,
+  reloadOnRestart: false,
+  notify: true,
+  scrollProportionally: true,
+  scrollThrottle: 0,
+  scrollRestoreTechnique: "window.name",
+  scrollElements: [],
+  scrollElementMapping: [],
+  reloadDelay: 0,
+  reloadDebounce: 0,
+  reloadThrottle: 0,
+  plugins: [],
+  injectChanges: true,
+  startPath: null,
+  minify: true,
+  host: null,
+  localOnly: false,
+  codeSync: true,
+  timestamps: true,
+  clientEvents: [
+    "scroll",
+    "scroll:element",
+    "input:text",
+    "input:toggles",
+    "form:submit",
+    "form:reset",
+    "click"
+  ],
+  socket: {
+    socketIoOptions: {
+      log: false
+    },
+    socketIoClientConfig: {
+      reconnectionAttempts: 50
+    },
+    path: "/browser-sync/socket.io",
+    clientPath: "/browser-sync",
+    namespace: "/browser-sync",
+    clients: {
+      heartbeatTimeout: 5000
+    }
+  },
+  tagNames: {
+    less: "link",
+    scss: "link",
+    css: "link",
+    jpg: "img",
+    jpeg: "img",
+    png: "img",
+    svg: "img",
+    gif: "img",
+    js: "script"
+  }
 };
