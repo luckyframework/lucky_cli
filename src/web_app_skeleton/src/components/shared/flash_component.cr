8101095d7e1e5b6f1b1d5993db15297e9b91bfa7
Paul Smith <paulcsmith0218@gmail.com>
2017-12-27 14:44:37 -0700

    Remove flash needs/expose
    Now that we pass the context every time, we can just call flash on the
    context.

--- src/web_app_skeleton/src/components/shared/flash_component.cr
+++ src/web_app_skeleton/src/components/shared/flash_component.cr
@@ -1,6 +1,6 @@
 module Shared::FlashComponent
   def render_flash
-    @flash.each do |flash_type, flash_message|
+    @context.flash.each do |flash_type, flash_message|
       div class: "flash-#{flash_type}" do
         text flash_message
       end
