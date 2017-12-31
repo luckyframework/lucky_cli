e7705ca8d184ce671c1e711afc7e2027f3d2d1f4
Paul Smith <paulcsmith0218@gmail.com>
2017-12-30 23:49:14 -0700

    Ensure process runner is installed

---
+++ src/ensure_process_runner_installed.cr
@@ -0,0 +1,21 @@
+class LuckyCli::EnsureProcessRunnerInstalled < LuckyCli::Task
+  include LuckyCli::TextHelpers
+
+  banner "Ensures that a process runner is installed"
+
+  def call
+    if LuckyCli::ProcessRunner.installed_process_runners.empty?
+      puts <<-ERROR
+      #{"Missing proccess runner for 'lucky dev'".colorize(:red)}
+
+      Install one of these...
+
+        #{green_arrow} Overmind (recommended): https://github.com/DarthSim/overmind#installation
+        #{green_arrow} Forego: https://github.com/ddollar/forego#installation
+        #{green_arrow} Heroku CLI: https://devcenter.heroku.com/articles/heroku-cli#download-and-install
+
+      ERROR
+      exit 1
+    end
+  end
+end
