c146efb8ef4c7c5491acee1ee1e10487e1d92a5f
mikeeus <itsmikias@gmail.com>
2018-04-01 20:11:00 +0300

    Add test for --api option WI

--- spec/integration/init_web_spec.cr
+++ spec/integration/init_web_spec.cr
@@ -1,18 +1,23 @@
 require "../spec_helper"
 
 describe "Initializing a new web project" do
-  it "creates the project successfully" do
+  it "creates a full web app successfully" do
     begin
-      puts "Running integration spec. This might take awhile...".colorize(:yellow)
+      puts "Web app: Running integration spec. This might take awhile...".colorize(:yellow)
       should_run_successfully "rm -rf ./test-project"
       should_run_successfully "crystal src/lucky.cr init test-project"
-      FileUtils.cd "test-project" do
-        should_run_successfully "bin/setup"
-        should_run_successfully "crystal build src/server.cr"
-        should_run_successfully "crystal build src/test_project.cr"
-        should_run_successfully "crystal src/app.cr"
-        should_run_successfully "crystal spec"
-      end
+      compile_test_project
+    ensure
+      FileUtils.rm_rf "test-project"
+    end
+  end
+
+  it "creates an api only web app successfully" do
+    begin
+      puts "Api Only: Running integration spec. This might take awhile...".colorize(:yellow)
+      should_run_successfully "rm -rf ./test-project"
+      should_run_successfully "crystal src/lucky.cr init test-project -D --api"
+      compile_test_project
     ensure
       FileUtils.rm_rf "test-project"
     end
@@ -32,6 +37,16 @@ describe "Initializing a new web project" do
     ensure
       FileUtils.rm_rf "test-project"
     end
+  end
+end
+
+private def compile_test_project
+  FileUtils.cd "test-project" do
+    should_run_successfully "bin/setup"
+    should_run_successfully "crystal build src/server.cr"
+    should_run_successfully "crystal build src/test_project.cr"
+    should_run_successfully "crystal src/app.cr"
+    should_run_successfully "crystal spec"
   end
 end
 
