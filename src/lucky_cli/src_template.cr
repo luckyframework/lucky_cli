1fa1a6cdcc6d1aa8ac7354fd7a0ff5427c693bb1
Paul Smith <paulcsmith0218@gmail.com>
2018-04-12 20:26:54 -0400

    Some tweaks and package.json changes

--- src/lucky_cli/src_template.cr
+++ src/lucky_cli/src_template.cr
@@ -3,11 +3,15 @@ require "random/secure"
 class SrcTemplate < Teeplate::FileTree
   directory "#{__DIR__}/../web_app_skeleton"
   getter project_name
-  getter api_only
+  getter? api_only
   getter crystal_project_name : String
 
   def initialize(@project_name : String, @api_only = false)
     @crystal_project_name = @project_name.gsub("-", "_")
+  end
+
+  private def browser?
+    !api_only?
   end
 
   def secret_key_base
