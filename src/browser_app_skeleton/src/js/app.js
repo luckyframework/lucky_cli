49c21952eaadfeaaf2fab45a2f9ccfec28d13d51
Paul Smith <paulcsmith0218@gmail.com>
2018-06-10 13:46:48 -0400

    Add note about RailsUjs and Turbolinks
    Closes #199

--- src/browser_app_skeleton/src/js/app.js
+++ src/browser_app_skeleton/src/js/app.js
@@ -1,5 +1,9 @@
 /* eslint no-console:0 */
+
+// RailsUjs is *required* for links in Lucky that use DELETE, POST and PUT.
 import RailsUjs from "rails-ujs";
+
+// Turbolinks is optional. Learn more: https://github.com/turbolinks/turbolinks/
 import Turbolinks from "turbolinks";
 
 RailsUjs.start();
