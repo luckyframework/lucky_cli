fd78ff9fae37b3dccc707220ffa88344e292db5d
Paul Smith <paulcsmith0218@gmail.com>
2018-08-14 20:22:03 -0400

    Defer for better performance. Use Turbolinks reload

--- src/browser_app_skeleton/src/components/shared/layout.cr
+++ src/browser_app_skeleton/src/components/shared/layout.cr
@@ -12,8 +12,8 @@ module Shared::Layout
     head do
       utf8_charset
       title "My App - #{page_title}"
-      css_link asset("css/app.css")
-      js_link asset("js/app.js")
+      css_link asset("css/app.css"), data_turbolinks_track: "reload"
+      js_link asset("js/app.js"), defer: "true", data_turbolinks_track: "reload"
       csrf_meta_tags
       responsive_meta_tag
     end
