edc9bc252dd07ab1fffba0a0d95e39f90693463e
Paul Smith <paulcsmith@users.noreply.github.com>
2019-01-04 23:35:00 -0500

    Remove unexpose (#278)

--- src/base_authentication_app_skeleton/src/actions/mixins/auth/redirect_if_signed_in.cr
+++ src/base_authentication_app_skeleton/src/actions/mixins/auth/redirect_if_signed_in.cr
@@ -2,7 +2,6 @@ module Auth::RedirectIfSignedIn
   macro included
     include Auth::SkipRequireSignIn
     before redirect_if_signed_in
-    unexpose current_user
   end
 
   private def redirect_if_signed_in
