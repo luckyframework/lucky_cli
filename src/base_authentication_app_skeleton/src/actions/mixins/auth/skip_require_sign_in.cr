e4b07895709547bc178bb6627fe82ddcef762c39
Paul Smith <paulcsmith0218@gmail.com>
2019-01-04 22:43:30 -0500

    Use skip macro
    Closes #273

--- src/base_authentication_app_skeleton/src/actions/mixins/auth/skip_require_sign_in.cr
+++ src/base_authentication_app_skeleton/src/actions/mixins/auth/skip_require_sign_in.cr
@@ -1,6 +1,6 @@
 module Auth::SkipRequireSignIn
-  def require_sign_in
-    continue
+  macro included
+    skip require_sign_in
   end
 
   # Since sign in is not required, current_user might be nil
