581f47ac9e49d85f5de32482e4f1021d49da01ac
Paul Smith <paulcsmith@users.noreply.github.com>
2019-02-24 23:41:35 -0800

    Use new logger (#299)

--- src/web_app_skeleton/config/log_handler.cr
+++
@@ -1,4 +0,0 @@
-Lucky::LogHandler.configure do |settings|
-  settings.show_timestamps = Lucky::Env.production?
-  settings.enabled = !Lucky::Env.test?
-end
---
+++ src/web_app_skeleton/config/logger.cr
@@ -0,0 +1,40 @@
+require "file_utils"
+
+logger =
+  if Lucky::Env.test?
+    # Logs to `tmp/test.log` so you can see what's happening without having
+    # a bunch of log output in your specs results.
+    FileUtils.mkdir("tmp")
+    Lucky::Logger.new(
+      io: File.new("tmp/test.log", mode: "w"),
+      level: Logger::Severity::DEBUG,
+      log_formatter: Lucky::PrettyLogFormatter.new
+    )
+  elsif Lucky::Env.production?
+    # This sets the log formatter to JSON so you can parse the logs with
+    # services like Logentries or Logstash.
+    #
+    # If you want pretty logs like in develpoment, remove the `log_formatter`
+    # from this call.
+    Lucky::Logger.new(
+      io: STDOUT,
+      level: Logger::Severity::INFO,
+      log_formatter: Lucky::JsonLogFormatter.new
+    )
+  else
+    # For development, log everything to STDOUT with the default pretty
+    # formatter.
+    Lucky::Logger.new(
+      io: STDOUT,
+      level: Logger::Severity::DEBUG,
+      log_formatter: Lucky::PrettyLogFormatter.new
+    )
+  end
+
+Lucky.configure do |settings|
+  settings.logger = logger
+end
+
+Lucky::LogHandler.configure do |settings|
+  settings.logger = logger
+end
