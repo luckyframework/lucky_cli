193bb1f2766962a47463e67416d22683f25ce6ad
Paul Smith <paulcsmith0218@gmail.com>
2019-02-25 18:42:42 -0800

    Use Dexter for the logger

--- src/web_app_skeleton/config/logger.cr
+++ src/web_app_skeleton/config/logger.cr
@@ -5,34 +5,30 @@ logger =
     # Logs to `tmp/test.log` so you can see what's happening without having
     # a bunch of log output in your specs results.
     FileUtils.mkdir("tmp")
-    Lucky::Logger.new(
+    Dexter::Logger.new(
       io: File.new("tmp/test.log", mode: "w"),
       level: Logger::Severity::DEBUG,
-      log_formatter: Lucky::PrettyLogFormatter.new
+      log_formatter: Lucky::PrettyLogFormatter
     )
   elsif Lucky::Env.production?
     # This sets the log formatter to JSON so you can parse the logs with
     # services like Logentries or Logstash.
     #
     # If you want logs like in develpoment use `Lucky::PrettyLogFormatter`.
-    Lucky::Logger.new(
+    Dexter::Logger.new(
       io: STDOUT,
       level: Logger::Severity::INFO,
-      log_formatter: Lucky::JsonLogFormatter.new
+      log_formatter: Dexter::Formatters::JsonLogFormatter
     )
   else
-    # For development, log everything to STDOUT with the pretty formatter
-    Lucky::Logger.new(
+    # For development, log everything to STDOUT with the pretty formatter.
+    Dexter::Logger.new(
       io: STDOUT,
       level: Logger::Severity::DEBUG,
-      log_formatter: Lucky::PrettyLogFormatter.new
+      log_formatter: Lucky::PrettyLogFormatter
     )
   end
 
 Lucky.configure do |settings|
-  settings.logger = logger
-end
-
-Lucky::LogHandler.configure do |settings|
   settings.logger = logger
 end
