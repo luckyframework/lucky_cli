cfd32619890848ad83b8d983a3bd612aeb96f649
Paul Smith <paulcsmith@users.noreply.github.com>
2019-02-27 19:19:48 -0800

    Make it clear backdoor is in test only (#307)
    It was scary to see "sign_in_through_backdoor" in your logs.
    
    This name change makes things shorter and also clarifies that the
    backdoor is for testing only.

--- src/base_authentication_app_skeleton/src/actions/mixins/auth/test_backdoor.cr
+++ src/base_authentication_app_skeleton/src/actions/mixins/auth/test_backdoor.cr
@@ -1,9 +1,9 @@
-module Auth::SignInThroughBackdoor
+module Auth::TestBackdoor
   macro included
-    before sign_in_through_backdoor
+    before test_backdoor
   end
 
-  private def sign_in_through_backdoor
+  private def test_backdoor
     if Lucky::Env.test? && (user_id = params.get?(:backdoor_user_id))
       user = UserQuery.find(user_id)
       sign_in user
