06823b261420a6ae7d47371e1b6cc9e5990cc818
Paul Smith <paulcsmith0218@gmail.com>
2019-03-01 15:22:16 -0800

    Remove unnecessary Authentic abstractions

--- src/base_authentication_app_skeleton/src/forms/password_reset_request_form.cr
+++ src/base_authentication_app_skeleton/src/forms/password_reset_request_form.cr
@@ -1,8 +1,20 @@
 class PasswordResetRequestForm < Avram::VirtualForm
-  include Authentic::FormHelpers
-  include FindAuthenticatable
+  # You can modify this in src/forms/mixins/user_from_email.cr
+  include UserFromEmail
 
   virtual email : String
+
+  # Run validations and yield the form and the user if valid
+  def submit
+    user = user_from_email
+    validate(user)
+
+    if valid?
+      yield self, user
+    else
+      yield self, nil
+    end
+  end
 
   def validate(user : User?)
     validate_required email
