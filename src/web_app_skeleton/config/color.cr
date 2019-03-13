049eb4030db095edd2e79e8bfb610535b2f07a74
Jeremy Woertink <jeremywoertink@gmail.com>
2019-03-13 14:30:55 -0700

    move the color option to logger since that's really what's being configured here

--- src/web_app_skeleton/config/color.cr
+++
@@ -1,4 +0,0 @@
-# This enables the color output when in development
-# Check out the Colorize docs for more information
-# https://crystal-lang.org/api/Colorize.html
-Colorize.enabled = Lucky::Env.development?
