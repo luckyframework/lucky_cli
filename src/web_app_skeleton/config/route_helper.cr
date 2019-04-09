4daa57786af3d80fbcc0af55416464456cd49b16
Paul Smith <paulcsmith@users.noreply.github.com>
2019-04-08 21:22:11 -0400

    Use localhost for the RouteHelper
    This is what you want most of the time, especially when using for callback Urls with OAuth apps

--- src/web_app_skeleton/config/route_helper.cr
+++ src/web_app_skeleton/config/route_helper.cr
@@ -5,6 +5,6 @@ Lucky::RouteHelper.configure do |settings|
     settings.base_uri = ENV.fetch("APP_DOMAIN")
   else
     # Set domain to the default host/port in development/test
-    settings.base_uri = "http://#{Lucky::ServerSettings.host}:#{Lucky::ServerSettings.port}"
+    settings.base_uri = "http://localhost:#{Lucky::ServerSettings.port}"
   end
 end
