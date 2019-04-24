cdf3d9d96519f4916927c38933a17e9b67d7d8b4
Jeremy Woertink <jeremywoertink@gmail.com>
2019-04-24 12:14:04 -0700

    move the dotenv loading before shards are loaded

--- src/web_app_skeleton/src/shards.cr
+++ src/web_app_skeleton/src/shards.cr
@@ -1,5 +1,8 @@
-# Require your shards here
+# Load .env file before any other config or app code
 require "dotenv"
+Dotenv.load
+
+# Require your shards here
 require "avram"
 require "lucky"
 require "carbon"
