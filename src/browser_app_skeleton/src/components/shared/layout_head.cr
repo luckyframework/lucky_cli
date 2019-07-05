e5a6e29997840a56ce67ef551e712c28ff03f5f1
Paul Smith <paulcsmith0218@gmail.com>
2019-07-05 12:14:06 -0400

    Disable turbolinks cache by default
    Closes #357

--- src/browser_app_skeleton/src/components/shared/layout_head.cr
+++ src/browser_app_skeleton/src/components/shared/layout_head.cr
@@ -8,6 +8,7 @@ class Shared::LayoutHead < BaseComponent
       title "My App - #{@page_title}"
       css_link asset("css/app.css"), data_turbolinks_track: "reload"
       js_link asset("js/app.js"), defer: "true", data_turbolinks_track: "reload"
+      meta name: "turbolinks-cache-control", content: "no-cache"
       csrf_meta_tags
       responsive_meta_tag
     end
