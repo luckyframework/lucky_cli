f89f1f4196408a1924c123eb6b53635946741870
Paul Smith <paulcsmith0218@gmail.com>
2019-07-05 12:17:44 -0400

    Clarify how to attach events and RailsUjs

--- src/browser_app_skeleton/src/js/app.js
+++ src/browser_app_skeleton/src/js/app.js
@@ -1,6 +1,7 @@
 /* eslint no-console:0 */
 
 // RailsUjs is *required* for links in Lucky that use DELETE, POST and PUT.
+// Though it says "Rails" it actually works with any framework.
 import RailsUjs from "rails-ujs";
 
 // Turbolinks is optional. Learn more: https://github.com/turbolinks/turbolinks/
@@ -8,3 +9,9 @@ import Turbolinks from "turbolinks";
 
 RailsUjs.start();
 Turbolinks.start();
+
+// If using Turbolinks, you can attach events to page load like this:
+//
+// document.addEventListener("turbolinks:load", function() {
+//   ...
+// })
