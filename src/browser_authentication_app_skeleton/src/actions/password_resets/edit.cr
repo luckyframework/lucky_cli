b6df860318d38a4b07d5a8090128a7093e98d077
Sébastien Nadon <sn@sebastiennadon.com>
2019-12-03 18:35:23 -0500

    Use Int64 for password_resets user_id (#439)

--- src/browser_authentication_app_skeleton/src/actions/password_resets/edit.cr
+++ src/browser_authentication_app_skeleton/src/actions/password_resets/edit.cr
@@ -3,6 +3,6 @@ class PasswordResets::Edit < BrowserAction
   include Auth::PasswordResets::TokenFromSession
 
   get "/password_resets/:user_id/edit" do
-    html NewPage, operation: ResetPassword.new, user_id: user_id.to_i
+    html NewPage, operation: ResetPassword.new, user_id: user_id.to_i64
   end
 end
