d94f80b62ee5cb68864c8c2bbeed5b5372842b7c
Paul Smith <paulcsmith0218@gmail.com>
2020-03-11 11:44:43 -0400

    Only add TestBackdoor in test env
    This way you won't see "test_backdoor" for every action in the logs.
    That was something that probably makes people nervous

--- src/browser_authentication_app_skeleton/src/actions/mixins/auth/test_backdoor.cr
+++ src/browser_authentication_app_skeleton/src/actions/mixins/auth/test_backdoor.cr
@@ -1,13 +1,15 @@
 module Auth::TestBackdoor
-  macro included
-    before test_backdoor
-  end
-
-  private def test_backdoor
-    if Lucky::Env.test? && (user_id = params.get?(:backdoor_user_id))
-      user = UserQuery.find(user_id)
-      sign_in user
+  {% if env["LUCKY_ENV"]? == "test" %}
+    macro included
+      before test_backdoor
     end
-    continue
-  end
+
+    private def test_backdoor
+      if user_id = params.get?(:backdoor_user_id)
+        user = UserQuery.find(user_id)
+        sign_in user
+      end
+      continue
+    end
+  {% end %}
 end
