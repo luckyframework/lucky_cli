0b2f8bf859dd693b09e3c9ef658cdb1ac6c0c697
Paul Smith <paulcsmith0218@gmail.com>
2020-03-24 16:17:33 -0400

    Add default with comments for pipe log
    Part of: https://github.com/luckyframework/lucky/pull/1062

--- src/web_app_skeleton/config/logger.cr
+++ src/web_app_skeleton/config/logger.cr
@@ -33,6 +33,20 @@ Lucky.configure do |settings|
   settings.logger = logger
 end
 
+Lucky::Action.configure do |settings|
+  # By default Lucky only logs when before/after pipes halt by redirecting, or
+  # rendering a response.
+  #
+  # Set 'pipe_log_level' to a 'Logger::Severity' to log whenever any pipe runs.
+  #
+  # For example:
+  #
+  #    settings.pipe_log_level = Logger::Severity::DEBUG
+  #
+  # Full list of log levels: https://crystal-lang.org/api/Logger/Severity.html#enum-members
+  settings.pipe_log_level = nil
+end
+
 Lucky::LogHandler.configure do |settings|
   # Skip logging static assets in development
   if Lucky::Env.development?
