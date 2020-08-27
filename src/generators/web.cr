2798240ce393ac86e468c940dd9e424ac052959e
Jeremy Woertink <jeremywoertink@gmail.com>
2020-08-27 13:22:53 -0700

    Generated projected now use Avram::Credentials. Fixes #537

--- src/generators/web.cr
+++ src/generators/web.cr
@@ -153,7 +153,7 @@ class LuckyCli::Generators::Web
     dependencies:
       lucky:
         github: luckyframework/lucky
-        version: ~> 0.23.0
+        branch: master
       authentic:
         github: luckyframework/authentic
         version: ~> 0.6.1
