c59cdf4f82e1f909a2592d0eedcd5065434b2daf
Jeremy Woertink <jeremywoertink@gmail.com>
2020-11-05 13:23:32 -0800

    Adding a cleanup method for integration specs to ensure the generated DB is also dropped as well as removing all generated project files. Fixes #578

--- spec/integration/init_web_spec.cr
+++ spec/integration/init_web_spec.cr
@@ -2,72 +2,85 @@ require "../spec_helper"
 
 include ShouldRunSuccessfully
 
-Spec.before_each do
-  FileUtils.rm_rf("test-project")
-end
-
 describe "Initializing a new web project" do
   it "creates a full web app successfully" do
     puts "Web app: Running integration spec. This might take awhile...".colorize(:yellow)
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project"
-    FileUtils.cp("spec/support/cat.gif", "test-project/public/assets/images/")
+    with_project_cleanup do
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project"
+      FileUtils.cp("spec/support/cat.gif", "test-project/public/assets/images/")
 
-    File.delete("test-project/.env")
-    compile_and_run_specs_on_test_project
-    File.read("test-project/.travis.yml").should contain "postgresql"
-    File.read(".github/workflows/ci.yml").should contain "postgres"
-    File.read("test-project/public/mix-manifest.json").should contain "images/cat.gif"
-    File.exists?("test-project/public/favicon.ico").should eq true
-    File.exists?("test-project/.env").should eq true
+      # Testing that dashes are translated to underscores
+      shard_yml_name = File.read_lines("test-project/shard.yml").find { |line| line =~ /^name:/ }
+      shard_yml_name.should eq("name: test_project")
+      File.exists?("test-project/src/test_project.cr").should be_true
+
+      File.delete("test-project/.env")
+      compile_and_run_specs_on_test_project
+      File.read("test-project/.travis.yml").should contain "postgresql"
+      File.read(".github/workflows/ci.yml").should contain "postgres"
+      File.read("test-project/public/mix-manifest.json").should contain "images/cat.gif"
+      File.exists?("test-project/public/favicon.ico").should eq true
+      File.exists?("test-project/.env").should eq true
+    end
   end
 
   it "creates a full web app with generator" do
     puts "Web app generators: Running integration spec. This might take awhile...".colorize(:yellow)
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project"
+    with_project_cleanup do
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project"
 
-    FileUtils.cd "test-project" do
-      should_run_successfully "shards install --ignore-crystal-version"
-      should_run_successfully "lucky gen.action.api Api::Users::Show"
-      should_run_successfully "lucky gen.action.browser Users::Show"
-      should_run_successfully "lucky gen.migration CreateThings"
-      should_run_successfully "lucky gen.model User"
-      should_run_successfully "lucky gen.page Users::IndexPage"
-      should_run_successfully "lucky gen.component Users::Header"
-      should_run_successfully "lucky gen.resource.browser Comment title:String"
+      FileUtils.cd "test-project" do
+        should_run_successfully "shards install --ignore-crystal-version"
+        should_run_successfully "lucky gen.action.api Api::Users::Show"
+        should_run_successfully "lucky gen.action.browser Users::Show"
+        should_run_successfully "lucky gen.migration CreateThings"
+        should_run_successfully "lucky gen.model User"
+        should_run_successfully "lucky gen.page Users::IndexPage"
+        should_run_successfully "lucky gen.component Users::Header"
+        should_run_successfully "lucky gen.resource.browser Comment title:String"
 
-      File.read("src/actions/comments/index.cr").should contain "Comments::Index"
-      File.read("src/actions/api/users/show.cr").should_not be_nil
-      File.read("src/actions/users/show.cr").should_not be_nil
-      File.read("src/pages/users/index_page.cr").should_not be_nil
-      File.read("src/components/users/header.cr").should contain "Users::Header < BaseComponent"
-      should_run_successfully "crystal build src/start_server.cr"
+        File.read("src/actions/comments/index.cr").should contain "Comments::Index"
+        File.read("src/actions/api/users/show.cr").should_not be_nil
+        File.read("src/actions/users/show.cr").should_not be_nil
+        File.read("src/pages/users/index_page.cr").should_not be_nil
+        File.read("src/components/users/header.cr").should contain "Users::Header < BaseComponent"
+        should_run_successfully "crystal build src/start_server.cr"
+      end
     end
   end
 
   it "creates an api only web app successfully" do
     puts "Api only: Running integration spec. This might take awhile...".colorize(:yellow)
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --api"
-    compile_and_run_specs_on_test_project
+    with_project_cleanup do
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --api"
+      compile_and_run_specs_on_test_project
+    end
   end
 
   it "creates an api only app without auth" do
     puts "Api only without auth: Running integration spec. This might take awhile...".colorize(:yellow)
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --api --no-auth"
-    compile_and_run_specs_on_test_project
+    with_project_cleanup do
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --api --no-auth"
+      compile_and_run_specs_on_test_project
+    end
   end
 
   it "creates a full app without auth" do
     puts "Web app without auth: Running integration spec. This might take awhile...".colorize(:yellow)
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --no-auth"
-    compile_and_run_specs_on_test_project
+    with_project_cleanup do
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --no-auth"
+      compile_and_run_specs_on_test_project
+    end
   end
 
   it "creates a full app in a different directory" do
     puts "Web app with custom directory: Running integration spec.".colorize(:yellow)
-    FileUtils.mkdir_p "/tmp/home/bob"
-    should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --dir /tmp/home/bob"
-    FileUtils.cd "/tmp/home/bob/test-project" do
-      File.read("src/shards.cr").should contain "lucky"
+    with_project_cleanup(project_directory: "/tmp/home/bob/test-project") do
+      FileUtils.mkdir_p "/tmp/home/bob"
+      should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --dir /tmp/home/bob"
+      FileUtils.cd "/tmp/home/bob/test-project" do
+        File.read("src/shards.cr").should contain "lucky"
+      end
     end
   end
 
@@ -106,20 +119,6 @@ describe "Initializing a new web project" do
     message = "Projects cannot be named app, app_database, app_server, shards, start_server."
     output.to_s.strip.should contain(message)
   end
-
-  it "translates dashes to underscores in the project name" do
-    output = IO::Memory.new
-    Process.run(
-      "crystal run src/lucky.cr -- init.custom test-project",
-      env: ENV.to_h,
-      output: output,
-      shell: true
-    )
-
-    shard_yml_name = File.read_lines("test-project/shard.yml").find { |line| line =~ /^name:/ }
-    shard_yml_name.should eq("name: test_project")
-    File.exists?("test-project/src/test_project.cr").should be_true
-  end
 end
 
 private def compile_and_run_specs_on_test_project
@@ -132,4 +131,19 @@ private def compile_and_run_specs_on_test_project
     should_run_successfully "crystal build src/start_server.cr"
     should_run_successfully "crystal spec"
   end
+end
+
+private def with_project_cleanup(project_directory = "test-project")
+  root_directory = FileUtils.pwd
+  yield
+  output = IO::Memory.new
+  Process.run(
+    "lucky db.drop",
+    env: ENV.to_h,
+    output: output,
+    shell: true
+  )
+  output.to_s.should contain("Done dropping")
+  FileUtils.cd root_directory
+  FileUtils.rm_rf project_directory
 end
