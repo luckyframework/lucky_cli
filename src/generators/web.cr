356b8fa53b8434bbbf7ba95e758c96b0abde15d9
Jeremy Woertink <jeremywoertink@gmail.com>
2020-12-15 13:25:53 -0800

    Release 0.25.0 (#588)
    * Release 0.25.0
    
    * trying to force GH Actions to run
    
    * Update version to actual releases
    
    * bump authentic

--- src/generators/web.cr
+++ src/generators/web.cr
@@ -153,10 +153,10 @@ class LuckyCli::Generators::Web
     dependencies:
       lucky:
         github: luckyframework/lucky
-        version: ~> 0.24.0
+        version: ~> 0.25.0
       authentic:
         github: luckyframework/authentic
-        version: ~> 0.7.0
+        version: ~> 0.7.1
       carbon:
         github: luckyframework/carbon
         version: ~> 0.1.2
@@ -169,7 +169,7 @@ class LuckyCli::Generators::Web
       append_text to: "shard.yml", text: <<-DEPS_LIST
         lucky_flow:
           github: luckyframework/lucky_flow
-          version: ~> 0.7.0
+          version: ~> 0.7.1
       DEPS_LIST
     end
 
