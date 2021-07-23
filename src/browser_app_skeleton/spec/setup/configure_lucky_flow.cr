24a28d528af3e0cf49468fe76627999b1a3822c0
Stephen Dolan <dolan.stephen1@gmail.com>
2021-07-23 13:00:22 -0400

    Fix duplicate comment markers

--- src/browser_app_skeleton/spec/setup/configure_lucky_flow.cr
+++ src/browser_app_skeleton/spec/setup/configure_lucky_flow.cr
@@ -5,9 +5,9 @@ LuckyFlow.configure do |settings|
   settings.stop_retrying_after = 200.milliseconds
   settings.base_uri = Lucky::RouteHelper.settings.base_uri
 
-  # # By default, LuckyFlow is set in "headless" mode (no browser window shown).
-  # # Uncomment this to enable running `LuckyFlow` in a Google Chrome window instead.
-  # # Be sure to disable for CI.
+  # By default, LuckyFlow is set in "headless" mode (no browser window shown).
+  # Uncomment this to enable running `LuckyFlow` in a Google Chrome window instead.
+  # Be sure to disable for CI.
   # settings.driver = LuckyFlow::Drivers::Chrome
 end
 Spec.before_each { LuckyFlow::Server::INSTANCE.reset }
