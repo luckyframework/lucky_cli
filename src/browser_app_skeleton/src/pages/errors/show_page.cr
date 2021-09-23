6b7f877b47fbe3d21a0cf15c6965095e7e3d2ed8
Jeremy Woertink <jeremywoertink@gmail.com>
2021-09-22 17:16:08 -0700

    Using html_with_status on the error page. Updated the needs variable to not conflict with the named_arg in that marco. Fixes #678

--- src/browser_app_skeleton/src/pages/errors/show_page.cr
+++ src/browser_app_skeleton/src/pages/errors/show_page.cr
@@ -2,7 +2,7 @@ class Errors::ShowPage
   include Lucky::HTMLPage
 
   needs message : String
-  needs status : Int32
+  needs status_code : Int32
 
   def render
     html_doctype
@@ -17,7 +17,7 @@ class Errors::ShowPage
 
       body do
         div class: "container" do
-          h2 status, class: "status-code"
+          h2 status_code, class: "status-code"
           h1 message, class: "message"
 
           ul class: "helpful-links" do
