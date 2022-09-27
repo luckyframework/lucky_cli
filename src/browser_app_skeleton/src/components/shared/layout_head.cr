280d2b6c44b11b41b79854a722f610e512c730c7
Jeremy Woertink <jeremywoertink@gmail.com>
2022-09-27 14:34:53 -0700

    Ripping out Turbolinks now that it's been deprecated and archived. Ref #629

--- src/browser_app_skeleton/src/components/shared/layout_head.cr
+++ src/browser_app_skeleton/src/components/shared/layout_head.cr
@@ -5,9 +5,8 @@ class Shared::LayoutHead < BaseComponent
     head do
       utf8_charset
       title "My App - #{@page_title}"
-      css_link asset("css/app.css"), data_turbolinks_track: "reload"
-      js_link asset("js/app.js"), defer: "true", data_turbolinks_track: "reload"
-      meta name: "turbolinks-cache-control", content: "no-cache"
+      css_link asset("css/app.css")
+      js_link asset("js/app.js"), defer: "true"
       csrf_meta_tags
       responsive_meta_tag
 
