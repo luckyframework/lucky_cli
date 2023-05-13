5d9ddb9c19e7eb22162ec06d5687734d701f5a0f
Jeremy Woertink <jeremywoertink@gmail.com>
2023-05-12 18:01:49 -0700

    Use OptionParser for the lucky CLI (#771)
    * Adding in option parsing so we can pull up help menus and see which default commands are available
    
    * make sure we can still list the available tasks when you're in an application
    
    * Refactor the CLI options to change depending on if you're in a Lucky app directory. Now shows the correct commands

--- src/lucky.cr
+++ src/lucky.cr
@@ -1,4 +1,5 @@
 require "colorize"
+require "option_parser"
 require "./lucky_cli/spinner"
 require "./lucky_cli"
 require "./generators/*"
@@ -9,6 +10,7 @@ include LuckyTask::TextHelpers
 
 args = ARGV.join(" ")
 tasks_file = ENV.fetch("LUCKY_TASKS_FILE", "./tasks.cr")
+inside_app_directory = File.file?(tasks_file)
 
 private def task_name : String?
   ARGV.first?
@@ -25,14 +27,113 @@ private def precompiled_task_path : String?
   end
 end
 
-if task_name == "dev"
-  LuckyCli::Dev.new.call
-elsif task_name == "init"
+private def print_lucky_app_not_detected_error
+  puts <<-ERROR
+
+  #{"You are not in a Lucky project".colorize(:red)}
+
+  Try this...
+
+    #{red_arrow} Run #{"lucky init".colorize(:green)} to create a Lucky project.
+    #{red_arrow} Change your project's root directory to see what tasks are available.
+
+  ERROR
+end
+
+run_dev_server = false
+start_wizard = false
+generate_custom_app = false
+
+options = OptionParser.new do |parser|
+  parser.banner = "Usage: lucky [command]"
+
+  if inside_app_directory
+    parser.on("dev", "Boot the lucky development server") do
+      run_dev_server = true
+      parser.banner = "Usage: lucky dev"
+      parser.on("-h", "--help", "Display lucky dev help menu") {
+        puts <<-MESSAGE
+        Usage: lucky dev
+
+        Boot your Lucky application. Uses the Procfile.dev to
+        run each service. Edit this file to change which services
+        are booted.
+        MESSAGE
+        exit
+      }
+    end
+
+    parser.on("tasks", "Display a list of the tasks available in your app") {
+      # This passes the help flag to LuckyTask within your app to
+      # generate a list of available tasks, and bypasses the help
+      # flag for this parser
+      args = "-h"
+      parser.banner = "Usage: lucky tasks"
+      parser.on("-h", "--help", "Display the lucky tasks help menu") {
+        puts <<-MESSAGE
+        Usage: lucky tasks
+
+        Run this from within your Lucky application directory.
+        This will compile a list of all the available tasks in your
+        application.
+
+        To compile your tasks, build the tasks.cr file
+        > crystal build tasks.cr -o bin/tasks
+
+        MESSAGE
+        exit
+      }
+    }
+  else
+    parser.on("init", "Start the Lucky wizard") do
+      start_wizard = true
+      parser.banner = "Usage: lucky init"
+      parser.on("-h", "--help", "Display lucky init help menu") {
+        puts <<-MESSAGE
+        Usage: lucky init
+
+        Run this to start the new application wizard.
+        MESSAGE
+        exit
+      }
+    end
+
+    parser.on("init.custom", "Generate a new Lucky application") do
+      generate_custom_app = true
+      # This command handles its own CLI arg parsing
+      LuckyCli::InitCustom.run
+    end
+  end
+
+  parser.on("-v", "--version", "Show the version of the LuckyCLI") {
+    puts LuckyCli::VERSION
+    exit
+  }
+
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+
+  parser.invalid_option do |flag|
+    STDERR.puts "ERROR: #{flag} is not a valid option."
+    STDERR.puts parser
+    exit(1)
+  end
+end
+
+options.parse
+
+if run_dev_server
+  if File.file?("Procfile.dev")
+    LuckyCli::Dev.new.call
+  else
+    print_lucky_app_not_detected_error
+  end
+elsif start_wizard
   LuckyCli::Init.run
-elsif task_name == "init.custom"
-  LuckyCli::InitCustom.run
-elsif ["-v", "--version"].includes?(task_name)
-  puts LuckyCli::VERSION
+elsif generate_custom_app
+  # already running
 elsif task_precompiled?
   exit Process.run(
     precompiled_task_path.to_s,
@@ -42,18 +143,9 @@ elsif task_precompiled?
     output: STDOUT,
     error: STDERR
   ).exit_code
-elsif File.file?(tasks_file)
+elsif inside_app_directory
   # Run task from tasks.cr file since this task is not precompiled
   LuckyCli::BuildAndRunTask.call(tasks_file, args)
 else
-  puts <<-MISSING_TASKS_FILE
-
-  #{"You are not in a Lucky project".colorize(:red)}
-
-  Try this...
-
-    #{red_arrow} Run #{"lucky init".colorize(:green)} to create a Lucky project.
-    #{red_arrow} Change your project's root directory to see what tasks are available.
-
-  MISSING_TASKS_FILE
+  print_lucky_app_not_detected_error
 end
