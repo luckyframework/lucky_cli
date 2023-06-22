f974f5e3047aff3078e7a682a2ffb8cf5b4c8c57
Michael Wagner <mdwranger@gmail.com>
2023-06-22 09:04:09 -0400

    feat: wip base_auth_app_skeleton

--- src/lucky_cli/base_authentication_src_template.cr
+++ src/lucky_cli/base_authentication_src_template.cr
@@ -1,3 +1,45 @@
-class BaseAuthenticationSrcTemplate < Teeplate::FileTree
-  directory "#{__DIR__}/../base_authentication_app_skeleton"
+class BaseAuthenticationSrcTemplate
+  def render(path : Path)
+    LuckyTemplate.write!(path, template_folder)
+  end
+
+  def template_folder
+    LuckyTemplate.create_folder do |root_dir|
+      root_dir.add_file("config/authentic.cr") do |io|
+        ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/config/authentic.cr.ecr", io)
+      end
+      root_dir.add_folder("db/migrations") do |migrations_dir|
+        migrations_dir.add_file(".keep")
+        migrations_dir.add_file("00000000000001_create_users.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/db/migrations/00000000000001_create_users.cr.ecr", io)
+        end
+      end
+      root_dir.add_folder("spec/support") do |support_dir|
+        support_dir.add_file(".keep")
+        support_dir.add_file("factories/user_factory.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/spec/support/factories/user_factory.cr.ecr", io)
+        end
+      end
+      root_dir.insert_folder("src", src_folder)
+    end
+  end
+
+  def src_folder
+    LuckyTemplate.create_folder do |src_dir|
+      src_dir.add_file("models/user.cr") do |io|
+        ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/models/user.cr.ecr", io)
+      end
+      src_dir.add_file("queries/user_query.cr") do |io|
+        ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/queries/user_query.cr.ecr", io)
+      end
+      src_dir.add_folder("operations") do |ops_dir|
+        ops_dir.add_file(".keep")
+        # ...
+        ops_dir.add_folder("mixins") do |mixins_dir|
+          mixins_dir.add_file(".keep")
+          # ...
+        end
+      end
+    end
+  end
 end
