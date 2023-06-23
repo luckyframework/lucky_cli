0584711238b44c7e1631412a72bfd8a75408d89b
Michael Wagner <mdwranger@gmail.com>
2023-06-23 09:04:58 -0400

    feat: complete base_auth_app_skeleton
    fix: regression in generators/web

--- src/lucky_cli/base_authentication_src_template.cr
+++ src/lucky_cli/base_authentication_src_template.cr
@@ -34,10 +34,26 @@ class BaseAuthenticationSrcTemplate
       end
       src_dir.add_folder("operations") do |ops_dir|
         ops_dir.add_file(".keep")
-        # ...
+        ops_dir.add_file("request_password_reset.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/request_password_reset.cr.ecr", io)
+        end
+        ops_dir.add_file("reset_password.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/reset_password.cr.ecr", io)
+        end
+        ops_dir.add_file("sign_in_user.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/sign_in_user.cr.ecr", io)
+        end
+        ops_dir.add_file("sign_up_user.cr") do |io|
+          ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/sign_up_user.cr.ecr", io)
+        end
         ops_dir.add_folder("mixins") do |mixins_dir|
           mixins_dir.add_file(".keep")
-          # ...
+          mixins_dir.add_file("password_validations.cr") do |io|
+            ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/mixins/password_validations.cr.ecr", io)
+          end
+          mixins_dir.add_file("user_from_email.cr") do |io|
+            ECR.embed("#{__DIR__}/../base_authentication_app_skeleton/src/operations/mixins/user_from_email.cr.ecr", io)
+          end
         end
       end
     end
