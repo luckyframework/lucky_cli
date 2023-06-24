c7336e726b9411bae28f80a98a1e2cce21ad33a6
Michael Wagner <mdwranger@gmail.com>
2023-06-24 09:19:01 -0400

    feat: rewrote shard file gen for lucky_template

--- src/lucky_cli/shard_file_generator.cr
+++ src/lucky_cli/shard_file_generator.cr
@@ -1,4 +1,5 @@
 require "yaml"
+require "lucky_template"
 
 class ShardFileGenerator
   getter project_name : String
@@ -14,91 +15,113 @@ class ShardFileGenerator
   )
   end
 
-  private def project_base_dependencies
-    {
-      lucky:                   {github: "luckyframework/lucky", version: "~> 1.0.0"},
-      avram:                   {github: "luckyframework/avram", version: "~> 1.0.0"},
-      carbon:                  {github: "luckyframework/carbon", version: "~> 0.3.0"},
-      carbon_sendgrid_adapter: {github: "luckyframework/carbon_sendgrid_adapter", version: "~> 0.3.0"},
-      lucky_env:               {github: "luckyframework/lucky_env", version: "~> 0.1.4"},
-      lucky_task:              {github: "luckyframework/lucky_task", version: "~> 0.1.1"},
-    }
+  def render(path : Path)
+    LuckyTemplate.write!(path, template_folder)
   end
 
-  private def project_auth_dependencies
-    {
-      authentic: {github: "luckyframework/authentic", version: "~> 1.0.0"},
-      jwt:       {github: "crystal-community/jwt", version: "~> 1.6.0"},
-    }
-  end
-
-  private def project_browser_dev_dependencies
-    {
-      lucky_flow: {github: "luckyframework/lucky_flow", version: "~> 0.9.0"},
-    }
-  end
-
-  private def project_additional_dev_dependencies
-    {
-      lucky_sec_tester: {github: "luckyframework/lucky_sec_tester", version: "~> 0.2.0"},
-    }
-  end
-
-  private def add_dependency(yaml, dep_name, details)
-    yaml.scalar dep_name.to_s
-    yaml.mapping do
-      details.each do |key, value|
-        yaml.scalar key.to_s
-        yaml.scalar value.to_s
-      end
+  def template_folder
+    LuckyTemplate.create_folder do |top_dir|
+      top_dir.add_file("shard.yml", <<-YAML)
+      #{shard_info}
+      #{shard_deps}
+      #{shard_dev_deps}
+      YAML
     end
   end
 
-  def render(project_directory : String)
-    File.open(File.join(project_directory, "shard.yml"), "w") do |f|
-      YAML.build(f) do |yaml|
-        yaml.mapping do
-          yaml.scalar "name"
-          yaml.scalar project_name
-          yaml.scalar "version"
-          yaml.scalar "0.1.0"
-          yaml.scalar "crystal"
-          yaml.scalar ">= #{Crystal::VERSION}"
-          yaml.scalar "targets"
-          yaml.mapping do
-            yaml.scalar project_name
-            yaml.mapping do
-              yaml.scalar "main"
-              yaml.scalar File.join("src", "#{project_name}.cr")
-            end
-          end
-          yaml.scalar "dependencies"
-          yaml.mapping do
-            project_base_dependencies.each do |dep_name, details|
-              add_dependency(yaml, dep_name, details)
-            end
+  def shard_info : String
+    normalize_yaml({
+      "name"    => project_name,
+      "version" => "0.1.0",
+      "targets" => {
+        project_name => {
+          "main" => Path.new("src", "#{project_name}.cr").to_s,
+        },
+      },
+      "crystal" => ">= #{Crystal::VERSION}",
+    })
+  end
 
-            if generate_auth?
-              project_auth_dependencies.each do |dep_name, details|
-                add_dependency(yaml, dep_name, details)
-              end
-            end
-          end
-          yaml.scalar "development_dependencies"
-          yaml.mapping do
-            if browser?
-              project_browser_dev_dependencies.each do |dep_name, details|
-                add_dependency(yaml, dep_name, details)
-              end
-            end
-            if with_sec_tester?
-              project_additional_dev_dependencies.each do |dep_name, details|
-                add_dependency(yaml, dep_name, details)
-              end
-            end
-          end
-        end
-      end
+  def shard_deps
+    deps = project_base_deps
+    if generate_auth?
+      deps = deps.merge(project_auth_deps)
     end
+    normalize_yaml({"dependencies" => deps})
+  end
+
+  def shard_dev_deps
+    deps = {} of String => String
+    if browser?
+      deps = deps.merge(project_browser_dev_deps)
+    end
+    if with_sec_tester?
+      deps = deps.merge(project_additional_dev_deps)
+    end
+    normalize_yaml({"development_dependencies" => deps})
+  end
+
+  private def normalize_yaml(hash) : String
+    hash.to_yaml.lines[1..].join('\n')
+  end
+
+  private def project_base_deps
+    {
+      "lucky" => {
+        "github"  => "luckyframework/lucky",
+        "version" => "~> 1.0.0",
+      },
+      "avram" => {
+        "github"  => "luckyframework/avram",
+        "version" => "~> 1.0.0",
+      },
+      "carbon" => {
+        "github"  => "luckyframework/carbon",
+        "version" => "~> 0.3.0",
+      },
+      "carbon_sendgrid_adapter" => {
+        "github"  => "luckyframework/carbon_sendgrid_adapter",
+        "version" => "~> 0.3.0",
+      },
+      "lucky_env" => {
+        "github"  => "luckyframework/lucky_env",
+        "version" => "~> 0.1.4",
+      },
+      "lucky_task" => {
+        "github"  => "luckyframework/lucky_task",
+        "version" => "~> 0.1.1",
+      },
+    }
+  end
+
+  private def project_auth_deps
+    {
+      "authentic" => {
+        "github"  => "luckyframework/authentic",
+        "version" => "~> 1.0.0",
+      },
+      "jwt" => {
+        "github"  => "crystal-community/jwt",
+        "version" => "~> 1.6.0",
+      },
+    }
+  end
+
+  private def project_browser_dev_deps
+    {
+      "lucky_flow" => {
+        "github"  => "luckyframework/lucky_flow",
+        "version" => "~> 0.9.0",
+      },
+    }
+  end
+
+  private def project_additional_dev_deps
+    {
+      "lucky_sec_tester" => {
+        "github"  => "luckyframework/lucky_sec_tester",
+        "version" => "~> 0.2.0",
+      },
+    }
   end
 end
