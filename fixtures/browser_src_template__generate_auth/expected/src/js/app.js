ff53c85452731b1b4eed9fad0b33dff08097fde4
Michael Wagner <mdwranger@gmail.com>
2023-12-14 10:03:11 -0500

    fix: browser_src_template

--- fixtures/browser_src_template__generate_auth/expected/src/js/app.js
+++ fixtures/browser_src_template__generate_auth/expected/src/js/app.js
@@ -2,5 +2,6 @@
 
 // Rails Unobtrusive JavaScript (UJS) is *required* for links in Lucky that use DELETE, POST and PUT.
 // Though it says "Rails" it actually works with any framework.
-require("@rails/ujs").start();
+import Rails from "@rails/ujs";
+Rails.start();
 
