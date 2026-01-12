9b1f51c6198010185a581a7f06736c1de5495b72
Jeremy <jeremywoertink@gmail.com>
2026-01-12 12:07:46 -0800

    Fill in this spec with a bit more testing

--- spec/end_to_end/browser_no_auth_spec.cr
+++ spec/end_to_end/browser_no_auth_spec.cr
@@ -10,15 +10,24 @@ describe "Lucky CLI", tags: "end_to_end" do
         should_run_successfully "crystal run src/lucky.cr -- init.custom test-project --no-auth"
 
         FileUtils.cd "test-project" do
+          should_run_successfully "crystal script/setup.cr"
+          should_run_successfully "crystal build src/test_project.cr"
+          should_run_successfully "lucky gen.action.api Api::Users::Show"
+          should_run_successfully "lucky gen.action.browser Users::Show"
+          should_run_successfully "lucky gen.migration CreateThings"
+          should_run_successfully "lucky gen.model User"
+          should_run_successfully "lucky gen.page Users::IndexPage"
+          should_run_successfully "lucky gen.component Users::Header"
+          should_run_successfully "lucky gen.resource.browser Comment title:String"
+          should_run_successfully "lucky gen.task email.monthly_update"
+          should_run_successfully "lucky gen.secret_key"
+          should_run_successfully "crystal spec"
+
           {% if !flag?(:windows) %}
             # Due to formatter on Windows checking different line endings, this will report changes to all crystal files
             # We only need to test this on 1 OS to ensure things are good
             should_run_successfully "crystal tool format --check spec src config"
           {% end %}
-          should_run_successfully "crystal script/setup.cr"
-          should_run_successfully "crystal build src/test_project.cr"
-          should_run_successfully "crystal spec"
-          should_run_successfully "lucky tasks"
         end
       end
     end
